43fbfb7467432dc21ab54a098594cec904f6d94f
vinaymodi-galaxy <vinay.modi@galaxyweblinks.in>
2021-04-19 19:18:41 +0530

    design scene

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //     float someNumber = 1.5f;
+    //     int someholeNumber = 2;
+    //     string babble = "Hello friends";
+    //     bool fact = true;
+
+    public Rigidbody2D rb;
+
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-5, 0);
+        }
+    }
+
+
+    // private void  Start()
+    // {
+
+    //     if(fact == true)
+    //     {
+    //         print(babble + "someNumber: " + someNumber.ToString() + "someholeNumber: " + someholeNumber.ToString() );
+    //     }
+    // }
+
+
+
+
+}
