85947bd1885bd4b6eba3bf987d2b52af1e841fdb
vinaymodi-galaxy <vinay.modi@galaxyweblinks.in>
2021-04-21 19:36:15 +0530

    working on entity

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -9,30 +9,88 @@ public class PlayerController : MonoBehaviour
     //     string babble = "Hello friends";
     //     bool fact = true;
 
-    public Rigidbody2D rb;
+    private Rigidbody2D rb;
+    [SerializeField]
+    private Animator anim;
+    private enum State { idle, running, jumping, falling }
+    private State state = State.idle;
+    private Collider2D coli;
+    [SerializeField] private LayerMask ground;
 
 
+    private void Start()
+    {
+
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        coli = GetComponent<Collider2D>();
+    }
     private void Update()
     {
-        if (Input.GetKey(KeyCode.A))
+        float hDirection = Input.GetAxis("Horizontal");
+
+
+        // if (Input.GetKey(KeyCode.A))
+        if (hDirection < 0)
         {
             rb.velocity = new Vector2(-5, rb.velocity.y);
             transform.localScale = new Vector2(-1, 1);
+            // anim.SetBool("running", true);
         }
 
-        if (Input.GetKey(KeyCode.D))
+        // if (Input.GetKey(KeyCode.D))
+        if (hDirection > 0)
         {
             rb.velocity = new Vector2(5, rb.velocity.y);
             transform.localScale = new Vector2(1, 1);
+            // anim.SetBool("running", true);
 
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        else
         {
-            rb.velocity = new Vector2(rb.velocity.x, 10f);
+            // anim.SetBool("running", false);
+
         }
+
+        if (Input.GetButtonDown("Jump") && coli.IsTouchingLayers(ground))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 8f);
+            state = State.jumping;
+        }
+
+        velocityState();
+        anim.SetInteger("state", (int)state);
     }
 
+    private void velocityState()
+    {
+        if (state == State.jumping)
+        {
+
+            if (rb.velocity.y < .1f)
+            {
+                state = State.falling;
+            }
+        }
+
+        else if (state == State.falling)
+        {
+            if (coli.IsTouchingLayers(ground))
+            {
+                state = State.idle;
+            }
+        }
+
+        else if (Mathf.Abs(rb.velocity.x) > 2F)
+        {
+            state = State.running;
+        }
+        else
+        {
+            state = State.idle;
+        }
+    }
 
 
 
