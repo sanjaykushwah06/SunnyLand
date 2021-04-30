2b8ae238ee67a41500f11c2bcf2a863e4a28859f
vinaymodi-galaxy <vinay.modi@galaxyweblinks.in>
2021-04-30 18:54:20 +0530

    working on enemy and its animation

---
+++ Assets/Script/Enemy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    protected Animator anim;
+
+    protected virtual void Start()
+    {
+
+        anim = GetComponent<Animator>();
+
+    }
+    public void JumpedOn()
+    {
+        // Destroy(this.gameObject);
+
+        anim.SetTrigger("Death");
+    }
+
+    public void Death()
+    {
+        Destroy(this.gameObject);
+    }
+}
